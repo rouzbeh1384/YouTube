58e2fa504be67d251ffbd2538738a284c7e0ad13
rouzbeh1384 <r.84.asgari@gmail.com>
2024-06-12 19:09:19 +0330

    Start

---
+++ src/main/java/com/example/youtube/DataBase/DataBaseManager.java
@@ -0,0 +1,7 @@
+package com.example.youtube.DataBase;
+
+public class DataBaseManager {
+
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.youtube {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.youtube to javafx.fxml;
+    exports com.example.youtube;
+}
