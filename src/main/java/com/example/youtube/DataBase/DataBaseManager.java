4c4304b6167f61ee752bf4bcd063ea7856244211
rouzbeh1384 <r.84.asgari@gmail.com>
2024-06-13 15:14:36 +0330

    first diagram for my database  and in method i database start and end connection

--- src/main/java/com/example/youtube/DataBase/DataBaseManager.java
+++ src/main/java/com/example/youtube/DataBase/DataBaseManager.java
@@ -1,6 +1,49 @@
 package com.example.youtube.DataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataBaseManager {
+
+
+        Connection connection;
+        Statement statement;
+
+
+        /** Start connection in database  */
+        private void StrartConnection(){
+                try {
+                        Class.forName("com.mysql.cj.jdbc.Driver");
+                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hangman","root","");
+                        statement=connection.createStatement();
+
+                }catch (SQLException | ClassNotFoundException e){
+                        e.getMessage();
+                }
+        }
+
+        /**   End  connection in database  */
+        private void EncConnection(){
+                try {
+                        if (connection != null) {
+                                statement.close();
+                                connection.close();
+
+                        }
+                }catch (Exception e){
+                        e.getMessage();
+                }
+
+        }
+
+
+
+
+
+
+
 
 
 
