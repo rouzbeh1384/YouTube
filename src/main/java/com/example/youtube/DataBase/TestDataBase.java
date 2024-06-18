c348032456aa03e78225e97b7930ad94e914a6b7
rouzbeh1384 <r.84.asgari@gmail.com>
2024-06-18 18:44:37 +0330

    update

--- src/main/java/com/example/youtube/DataBase/TestDataBase.java
+++ src/main/java/com/example/youtube/DataBase/TestDataBase.java
@@ -1,5 +1,10 @@
 package com.example.youtube.DataBase;
 
+import com.example.youtube.Model.User;
+
+import static com.example.youtube.DataBase.DataBaseManager.getUser;
+import static com.example.youtube.DataBase.DataBaseManager.insertUser;
+
 public class TestDataBase {
 
     public static void main(String[] args) {
