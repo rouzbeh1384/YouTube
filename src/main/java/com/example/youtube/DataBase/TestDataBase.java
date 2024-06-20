6c344b2e3d52e26a91e54317588f46a974a53f44
rouzbeh1384 <r.84.asgari@gmail.com>
2024-06-21 01:53:05 +0330

    update

--- src/main/java/com/example/youtube/DataBase/TestDataBase.java
+++ src/main/java/com/example/youtube/DataBase/TestDataBase.java
@@ -1,14 +1,29 @@
 package com.example.youtube.DataBase;
 
-import com.example.youtube.Model.User;
+import com.example.youtube.Model.*;
 
-import static com.example.youtube.DataBase.DataBaseManager.getUser;
-import static com.example.youtube.DataBase.DataBaseManager.insertUser;
+import java.util.ArrayList;
+
+import static com.example.youtube.DataBase.DataBaseManager.*;
 
 public class TestDataBase {
 
     public static void main(String[] args) {
 
+
+//      Video a=  new Video("121","12","12","12","12","2000-01-01",12,12,12,12);
+//      a.setCategory("test");
+//      insertVideo(a);
+//        deleteUser("12","1");
+//        deleteVideo("12","12");
+//insertChanel(new Channel("12","roubee","this is a test","behrouz","sdasd.png"));
+//        deleteChanel("12");
+
+        insertComment(new Comment("12121","12","12","213123","2000-11-11",123,123),"12");
+//        if(!insertPLayList(new PlayList("re","ee","er","ererree"))){
+//            System.out.println("Not");
+//        }
+//        System.out.println(getList_video("12").size());
     }
 
 
