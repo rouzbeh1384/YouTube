9d83ffdef152792f5f01a2c32c24a8cb2cf60b54
rouzbeh1384 <r.84.asgari@gmail.com>
2024-06-21 23:44:52 +0330

    Test

--- src/main/java/com/example/youtube/DataBase/TestDataBase.java
+++ src/main/java/com/example/youtube/DataBase/TestDataBase.java
@@ -14,12 +14,25 @@ public class TestDataBase {
 //      Video a=  new Video("121","12","12","12","12","2000-01-01",12,12,12,12);
 //      a.setCategory("test");
 //      insertVideo(a);
+//        User b=new User("ro1h","r.84.askar12i@gmail.com","2005-03-03","12","Iran","12122");
+//        insertUser(b);
+//        b.changeUsername("bez");
+//        System.out.println(b.getUsername());
+//         UpdateUserInformation(b);
+//        Channel a=new Channel();
+//       a= getChannel("123",1);
+//        System.out.println(a.getDescription());
+//        a.changeDescription("this is test for chane the information ");
+//    UpdateChanelInfromation(a);
+
+        AddPlayList("!2","!@");
+//         deleteUser("12122","12");
 //        deleteUser("12","1");
 //        deleteVideo("12","12");
 //insertChanel(new Channel("12","roubee","this is a test","behrouz","sdasd.png"));
 //        deleteChanel("12");
 
-        insertComment(new Comment("12121","12","12","213123","2000-11-11",123,123),"12");
+//        insertComment(new Comment("12121","12","12","213123","2000-11-11",123,123),"12");
 //        if(!insertPLayList(new PlayList("re","ee","er","ererree"))){
 //            System.out.println("Not");
 //        }
